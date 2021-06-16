957b5a0559513a4256eee4ce4b61be2cffd3f793
joshua benker <joshuabenker@joshuas-MBP.fritz.box>
2021-06-16 15:23:14 +0200

    pep8

--- dippid_game.py
+++ dippid_game.py
@@ -1,4 +1,4 @@
-#script written by joshua benker
+# script written by joshua benker
 
 import sys
 import math
@@ -16,15 +16,16 @@ BODY_WIDTH = 100
 BODY_HEIGHT = 30
 
 
-#state of the game
+# state of the game
 class GameState(Enum):
     INTRO = 1
     START = 2
     LOST = 3
 
-#main game class
-#neccesssary: an android phone to get the dippid sensor output
-#creates a ball-object and two body-objects from the other classes
+
+# main game class
+# neccesssary: an android phone to get the dippid sensor output
+# creates a ball-object and two body-objects from the other classes
 class Game(QtWidgets.QWidget):
 
     sensor = ()
@@ -46,13 +47,13 @@ class Game(QtWidgets.QWidget):
         self.timer = QtCore.QTimer(self)
         self.xPos_bt = self.frameGeometry().width() / 2 - BODY_WIDTH / 2
         self.yPos_bt = self.frameGeometry().height() - BODY_HEIGHT
-        self.info = QtCore.QRect(0, self.frameGeometry().height() / 2,self.frameGeometry().width(), 100)
+        self.info = QtCore.QRect(0, self.frameGeometry().height() / 2, self.frameGeometry().width(), 100)
         self.points_box = QtCore.QRect(20, 10, self.frameGeometry().width(), 40)
         self.game_state = GameState.INTRO
         self.init_timer_game_loop()
         self.show()
-    
-    #initialize the painter and calls the info depending on the game state
+
+    # initialize the painter and calls the info depending on the game state
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
         self.draw_body(painter)
@@ -62,67 +63,67 @@ class Game(QtWidgets.QWidget):
             self.draw_intro_message(painter)
         elif self.game_state == GameState.LOST:
             self.draw_gamover_message(painter)
-  
-    #paints the bodies
+
+    # paints the bodies
     def draw_body(self, painter):
         painter.setBrush(QtGui.QBrush(QtCore.Qt.blue, QtCore.Qt.SolidPattern))
         painter.drawRect(self.body_bottom)
         painter.drawRect(self.body_top)
 
-    #paints the ball
+    # paints the ball
     def draw_ball(self, painter):
         painter.setBrush(QtGui.QBrush(QtCore.Qt.cyan, QtCore.Qt.SolidPattern))
         painter.setPen(QtGui.QPen(QtCore.Qt.cyan, QtCore.Qt.SolidPattern))
         painter.drawEllipse(self.ball.x, self.ball.y, self.ball.radius, self.ball.radius)
 
-    #paints the intro message
+    # paints the intro message
     def draw_intro_message(self, painter):
         painter.setPen(QtGui.QPen(QtCore.Qt.black, QtCore.Qt.SolidPattern))
         text = "Welcome.\nPress 'Button 1' to start.\nUse your phone to move " \
                "the two objects by tilting it. \n You are both players."
         painter.drawText(self.info, QtCore.Qt.AlignCenter, text)
 
-    #paints the game over message
+    # paints the game over message
     def draw_gamover_message(self, painter):
         text = "Oh damn. You missed the ball! \n " \
-                "You made " +str(self.points)+ " points \nPress 'Button 1' to restart."
+                "You made " + str(self.points) + " points \nPress 'Button 1' to restart."
         painter.drawText(self.info, QtCore.Qt.AlignCenter, text)
 
-    #paints the points the player achieves
-    def draw_points(self,painter):
+    # paints the points the player achieves
+    def draw_points(self, painter):
         painter.setPen(QtGui.QPen(QtCore.Qt.black, QtCore.Qt.SolidPattern))
         points = "Points: " + str(self.points)
         painter.drawText(self.points_box, QtCore.Qt.AlignLeft, points)
 
-    #initialize the sensor on port 5700
+    # initialize the sensor on port 5700
     def init_sensor(self):
         BUTTON_START = 'button_1'
         self.sensor = SensorUDP(SENSOR_PORT)
         self.sensor.register_callback(BUTTON_START, self.button_start_pressed)
 
-    #initialize the two bodies with the class 'Body'
+    # initialize the two bodies with the class 'Body'
     def init_body(self):
         self.xPos_bt = self.frameGeometry().width() / 2 - BODY_WIDTH / 2
         self.yPos_bt = self.frameGeometry().height() - BODY_HEIGHT
         self.body_bottom = Body(self.xPos_bt, self.yPos_bt, BODY_WIDTH, BODY_HEIGHT, self, 'bottom')
         self.body_top = Body(self.xPos_bt, 0, BODY_WIDTH, BODY_HEIGHT, self, 'top')
- 
-    #initialize the ball with the class 'Ball'
+
+    # initialize the ball with the class 'Ball'
     def init_ball(self):
         xPos = self.body_bottom.x() + self.body_bottom.body_width / 2
         yPos = self.body_bottom.y() - 25
         self.ball = Ball(xPos, yPos, 30, self)
 
-    #game loop
-    #found on https://doc.qt.io/qtforpython-5/PySide2/QtCore/QTimer.html
+    # game loop
+    # found on https://doc.qt.io/qtforpython-5/PySide2/QtCore/QTimer.html
     def init_timer_game_loop(self):
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.game_loop)
         self.timer.start(30)
 
-    #gets called when the 'button 1' got pressed
-    #changes the game state and restarts the game if it's over
-    def button_start_pressed(self,data):
+    # gets called when the 'button 1' got pressed
+    # changes the game state and restarts the game if it's over
+    def button_start_pressed(self, data):
         if data == 0:
             return
         if self.game_state == GameState.INTRO:
@@ -130,9 +131,8 @@ class Game(QtWidgets.QWidget):
         if self.game_state == GameState.LOST:
             self.game_state = GameState.START
             self.start_new_round()
-            
 
-    #game loop gets called by the timer
+    # game loop gets called by the timer
     def game_loop(self):
         if self.game_state == GameState.START:
             if self.sensor.has_capability('accelerometer'):
@@ -140,26 +140,27 @@ class Game(QtWidgets.QWidget):
             else:
                 return
             value_y = sensorVal['y']
-            self.body_bottom.move(value_y *7)
-            self.body_top.move(value_y *7)
+            self.body_bottom.move(value_y * 7)
+            self.body_top.move(value_y * 7)
             self.ball.move()
             self.update()
 
-    #gets called by the ball function is_gameover
-    #sets the game state to "lost"
+    # gets called by the ball function is_gameover
+    # sets the game state to "lost"
     def on_gameover(self):
         self.game_state = GameState.LOST
         self.update()
 
-    #gets called when the player starts a new round after he lost
+    # gets called when the player starts a new round after he lost
     def start_new_round(self):
         self.points = 0
-        self.body_bottom.moveTo(self.xPos_bt,self.yPos_bt)
+        self.body_bottom.moveTo(self.xPos_bt, self.yPos_bt)
         self.body_top.moveTo(self.xPos_bt, 0)
         self.init_ball()
 
-#class for the body
-#handles moving depending on the body-type (bottom or top body)
+
+# class for the body
+# handles moving depending on the body-type (bottom or top body)
 class Body(QtCore.QRect):
 
     def __init__(self, x, y, width, height, window, body_type):
@@ -183,8 +184,9 @@ class Body(QtCore.QRect):
             elif self.x() > self.window.frameGeometry().width() - self.body_width:
                 self.moveLeft(self.window.frameGeometry().width() - self.body_width)
 
-#class for the ball
-#handles moving and if it hits the window or the body
+
+# class for the ball
+# handles moving and if it hits the window or the body
 class Ball:
 
     def __init__(self, x, y, diameter, window):
@@ -196,7 +198,7 @@ class Ball:
         self.speed_x = 6
         self.speed_y = -6
 
-    #handles ball-moving
+    # handles ball-moving
     def move(self):
         self.x += self.speed_x
         self.y += self.speed_y
@@ -204,10 +206,10 @@ class Ball:
         self.hit_body()
         self.is_gameover()
 
-    #checks if ball hits the body
+    # checks if ball hits the body
     def hit_body(self):
         hit = self.touch_body(self.window.body_bottom)
-        if hit == False:
+        if hit is False:
             hit = self.touch_body(self.window.body_top)
         if hit == 1:
             self.speed_x *= -1
@@ -215,16 +217,14 @@ class Ball:
             self.window.points += 1
             self.speed_y *= -1
 
-    #checks if ball hits the window   
+    # checks if ball hits the window
     def hit_window(self):
         if self.x + self.radius * 2 > self.window.frameGeometry().width() or self.x <= 0:
             self.speed_x *= -1
-
         elif self.y <= 0:
             self.speed_y *= -1
 
-
-    #calculates the distance and returns where the ball hit the body 
+    # calculates the distance and returns where the ball hit the body
     def touch_body(self, rect):
         x_middle = self.x + self.radius
         y_middle = self.y + self.radius
@@ -242,7 +242,7 @@ class Ball:
 
         distance_x = x_middle - x_value
         distance_y = y_middle - y_value
-        distance = math.sqrt(distance_x **2 + distance_y **2)
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
 
         if distance <= self.radius:
             if distance_x == 0:
@@ -252,7 +252,7 @@ class Ball:
 
         return False
 
-    #checks if player is game over
+    # checks if player is game over
     def is_gameover(self):
         if self.y > self.window.frameGeometry().height() or self.y <= 0:
             self.window.on_gameover()
@@ -261,4 +261,3 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     game = Game()
     app.exec()
-
